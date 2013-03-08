85d38e542c1daf5d2daf03fc461656db231ce33a
Jeff Smith <jefmsmit@gmail.com>
2013-03-08 14:17:06 -0700

    updating to reflect usage of rails/generators

--- lib/gdshowsdb.rb
+++ lib/gdshowsdb.rb
@@ -3,6 +3,7 @@ require "gdshowsdb/version"
 require 'rubygems'
 require 'sqlite3'
 require 'active_record'
+require 'rails/generators'
 
 Dir[File.dirname(__FILE__) + '/gdshowsdb/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/gdshowsdb/db/migrations/*.rb'].each {|file| require file }
