799d4c080a3137d5cde9e07daf6d781680a6d414
Jeff Smith <jefmsmit@gmail.com>
2013-04-09 15:06:47 -0600

    updating show to have a to_s override

--- lib/gdshowsdb/models/show.rb
+++ lib/gdshowsdb/models/show.rb
@@ -1,23 +1,27 @@
 class Show < ActiveRecord::Base
-	include Extensions::UUID
+  include Extensions::UUID
 	
-	has_many :show_sets, :foreign_key => :show_uuid, :primary_key => :uuid
-	has_many :song_occurences, :foreign_key => :show_uuid, :primary_key => :uuid
-	has_many :song_refs, :through => :song_occurences, :foreign_key => :show_uuid
+  has_many :show_sets, :foreign_key => :show_uuid, :primary_key => :uuid
+  has_many :song_occurences, :foreign_key => :show_uuid, :primary_key => :uuid
+  has_many :song_refs, :through => :song_occurences, :foreign_key => :show_uuid
 
-	attr_accessible :uuid, :year, :month, :day, :venue, :city, :state, :country
+  attr_accessible :uuid, :year, :month, :day, :venue, :city, :state, :country
 
-	def date_string
-		"#{year}/#{pad month}/#{pad day}"
-	end
+  def date_string
+    "#{year}/#{pad month}/#{pad day}"
+  end
 
-	def title
-		"#{date_string} #{venue}, #{city}, #{state}, #{country}"
-	end
+  def title
+    "#{date_string} #{venue}, #{city}, #{state}, #{country}"
+  end
+  
+  def to_s
+   title
+  end
 
-	private 
+  private 
 
-	def pad(int)
-		"%02d" % int
-	end
+  def pad(int)
+    "%02d" % int
+  end
 end
