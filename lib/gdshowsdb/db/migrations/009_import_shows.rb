27b2baf86e3ba3f61e006a1816024589293fab02
Jeff Smith <jefmsmit@gmail.com>
2014-12-01 09:49:50 -0700

    slight refactor on the show import migration

--- lib/gdshowsdb/db/migrations/009_import_shows.rb
+++ lib/gdshowsdb/db/migrations/009_import_shows.rb
@@ -7,30 +7,9 @@ class ImportShows < ActiveRecord::Migration
 	
 	def up
 		(1965..1995).each do |year|
-			show_yaml_parser = ShowYAMLParser.from_yaml(year)			
-			show_yaml_parser.parse.each do |show_yaml|
-				Show.create(show_yaml)
-			end
-
-			set_yaml_parser = SetYAMLParser.from_yaml(year)
-			set_yaml_parser.parse.each do |set_yaml|
-				ShowSet.create(set_yaml) do |show_set|
-					show_set.show = Show.find_by_uuid(set_yaml[:show_uuid])
-				end
-			end
-
-			song_yaml_parser = SongYAMLParser.from_yaml(year)
-			song_yaml_parser.parse.each do |song_yaml|
-				created_song = Song.create(song_yaml) do |song|
-					song.show_set = ShowSet.find_by_uuid(song_yaml[:show_set_uuid])
-				end
-				song_ref = SongRef.find_by_name(song_yaml[:name])
-				song_ref.songs << Song.find_by_uuid(song_yaml[:uuid])
-				
-				song_ref.song_occurences.create(uuid: generate_uuid, position: song_yaml[:position]) do |occurence|
-					occurence.show = ShowSet.find_by_uuid(song_yaml[:show_set_uuid]).show					
-				end
-			end
+			create_shows(year)
+			create_sets(year)
+			create_songs(year)			
 		end
 	end
 
@@ -38,5 +17,43 @@ class ImportShows < ActiveRecord::Migration
 		Song.delete_all
 		ShowSet.delete_all
 		Show.delete_all
+	end
+
+	private
+
+	def create_shows(year)
+		show_yaml_parser = ShowYAMLParser.from_yaml(year)			
+		show_yaml_parser.parse.each do |show_yaml|
+			show = Show.create(show_yaml)
+			puts "Created #{show.title}"
+		end
 	end	
+
+	def create_sets(year)
+		set_yaml_parser = SetYAMLParser.from_yaml(year)
+		set_yaml_parser.parse.each do |set_yaml|
+			ShowSet.create(set_yaml) do |show_set|
+				show_set.show = Show.find_by_uuid(set_yaml[:show_uuid])
+			end
+		end
+	end
+
+	def create_songs(year)
+		song_yaml_parser = SongYAMLParser.from_yaml(year)
+		song_yaml_parser.parse.each do |song_yaml|
+			Song.create(song_yaml) do |song|
+				song.show_set = ShowSet.find_by_uuid(song_yaml[:show_set_uuid])
+			end
+			create_song_relationships(song_yaml)
+		end
+	end
+
+	def create_song_relationships(song_yaml)
+		song_ref = SongRef.find_by_name(song_yaml[:name])
+		song_ref.songs << Song.find_by_uuid(song_yaml[:uuid])
+		
+		song_ref.song_occurences.create(uuid: generate_uuid, position: song_yaml[:position]) do |occurence|
+			occurence.show = ShowSet.find_by_uuid(song_yaml[:show_set_uuid]).show					
+		end
+	end
 end
